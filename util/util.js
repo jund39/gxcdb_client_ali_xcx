58302d07e2817d37ffa02f3d17c62a2546370b5b
yinchangliao <changliao_yin@outlook.com>
2020-11-03 15:50:25 +0800

    修改文字等

--- util/util.js
+++ util/util.js
@@ -97,10 +97,10 @@ var i = getApp(),
     s = require("./md5.js"),
     r = require("./base64.js"),
     c = {
-  oid: 1,
-  ocode: "zhongyun",
-  host: "https://www.zhongyunke.com/app",
-  qrcodeurl: "https://www.zhongyunke.com",
+  oid: 7,
+  ocode: "chiyangzhineng",
+  host: "https://cms.chiyangzhineng.com/app",
+  qrcodeurl: "https://cms.chiyangzhineng.com",
   headerkey: "zhongyun@2020&8889"
 };
 
