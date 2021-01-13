85c5a8cb91d20b3cff1f76ca07e0f73f89a29490
yinchangliao <changliao_yin@outlook.com>
2021-01-13 16:45:38 +0800

    修改增加免押订单主动付款渠道，修改小程序客服电话等问题

--- pages/chargingTime/chargingTime.js
+++ pages/chargingTime/chargingTime.js
@@ -50,7 +50,7 @@ Page({
           t.alipayPayment(a.data.amount, 2, function (t) {
             e.setData({
               isClick: !0
-            }), e.goPayment();
+            }), e.checkStatus(t);
           }, function () {
             e.setData({
               isClick: !0
